c6180e6864ad7a8374a3d74e3840f51d29694db9
nihui <18202504057@163.com>
2019-02-12 14:46:41 +0800

    添加新内容

---
+++ Test/src/com/example/io/Test.java
@@ -0,0 +1,14 @@
+package com.example.io;
+
+/**
+ * @ClassName Test
+ * @Author nihui
+ * @Date 2019/2/12 14:45
+ * @Version 1.0
+ * @Description TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试IO操作");
+    }
+}
