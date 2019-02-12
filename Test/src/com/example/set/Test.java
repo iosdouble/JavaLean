958a9f9ce9b9a06edcf84f29bc7f91c6c914930e
nihui <18202504057@163.com>
2019-02-12 14:54:23 +0800

    添加测试集合类

---
+++ Test/src/com/example/set/Test.java
@@ -0,0 +1,14 @@
+package com.example.set;
+
+/**
+ * @ClassName Test
+ * @Author nihui
+ * @Date 2019/2/12 14:53
+ * @Version 1.0
+ * @Description TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试集合");
+    }
+}
